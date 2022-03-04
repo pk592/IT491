7224e7cb70108429c4e1117c4afc023e9d48e0a9
pk592 <pk592@njit.edu>
2022-03-04 16:16:18 -0500

    draft of login
    draft of login

---
+++ loginpage/login.php
@@ -0,0 +1,21 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<head>';
+echo '<style>';
+echo 'body{';
+echo 'background-color: black;';
+echo '}';
+echo 'h1{';
+echo 'font-family: Spartan;';
+echo 'color: #FFFFFF;';
+echo 'font-style: normal;';
+echo 'text-align: center;';
+echo '}';
+echo '</style>';
+echo '<title>Behind The Stick</title>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Behind The Stick</h1>';
+echo '';
+echo '</body>';
+?>
